2d0c3f8a687bbf0db748d85329dd437ec5d6840c
Srinjayee Paul <srinjayee04@gmail.com>
2025-07-14 00:04:50 +0530

    week4

---
+++ WEEK4/SimpleWebAPI/SimpleWebAPI/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SimpleWebAPI.Models;
+using System.Collections.Generic;
+
+namespace SimpleWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private static List<Product> products = new List<Product> {
+            new Product { Id = 1, Name = "Pen", Price = 10 },
+            new Product { Id = 2, Name = "Notebook", Price = 50 }
+        };
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(products);
+        }
+        [HttpPost]
+        public IActionResult AddProduct([FromBody] Product product)
+        {
+            products.Add(product);
+            return Ok(product);
+        }
+    }
+}
---
+++ WEEK4/SimpleWebAPI/SimpleWebAPI/Program.cs
@@ -0,0 +1,29 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSwagger",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseCors("AllowSwagger");
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.MapControllers();
+app.Run();
